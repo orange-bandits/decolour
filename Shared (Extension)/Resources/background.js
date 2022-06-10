19fe077e65853a5df7a0d7ab71577b77e5873b60
Tugkan Kibar <tugkan@orangebandits.com>
2022-06-11 00:17:37 +0200

    Add icons

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -3,12 +3,36 @@ const status = {};
 browser.browserAction.onClicked.addListener(() => {
     browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const tab = tabs[0];
-        status[tab.id] = !status[tab.id];
+        const isEnabled = !status[tab.id];
+        status[tab.id] = isEnabled;
         browser.tabs.insertCSS(tab.id, {
             cssOrigin: 'user',
             file: 'grayscale.css',
         }).then(() => {
-            browser.tabs.executeScript(tab.id, { file: 'toggle-grayscale.js'
+            browser.tabs.executeScript(tab.id, { file: 'toggle-grayscale.js' }).then(() => {
+                if (isEnabled) {
+                    browser.browserAction.setIcon({
+                        path: {
+                            "16": "images/grayscale/toolbar-icon-16.png",
+                            "19": "images/grayscale/toolbar-icon-19.png",
+                            "32": "images/grayscale/toolbar-icon-32.png",
+                            "38": "images/grayscale/toolbar-icon-38.png",
+                            "48": "images/grayscale/toolbar-icon-48.png",
+                            "72": "images/grayscale/toolbar-icon-72.png"
+                        }
+                    });
+                } else {
+                    browser.browserAction.setIcon({
+                        path: {
+                            "16": "images/coloured/toolbar-icon-16.png",
+                            "19": "images/coloured/toolbar-icon-19.png",
+                            "32": "images/coloured/toolbar-icon-32.png",
+                            "38": "images/coloured/toolbar-icon-38.png",
+                            "48": "images/coloured/toolbar-icon-48.png",
+                            "72": "images/coloured/toolbar-icon-72.png"
+                        }
+                    });
+                }
             });
         });
     })
